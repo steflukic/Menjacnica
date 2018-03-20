872c6d6f18a86ec943d8c218ef132ef87cbc7569
User <User@192.168.40.246>
2018-03-20 22:13:46 +0100

    Napravljen je paket menjacnica sa dve klase: Menjacnica i Valuta. Klasa Valuta sadrzi privatne atribute naziv, skracenica, prodajni, kupovni, srednji, datum; kao i odgovarajuce getter i setter metode. Klasa Menjacnica sadrzi privatnu praznu listu objekata klase Valuta.

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,9 @@
+package menjacnica;
+
+import java.util.LinkedList;
+
+public class Menjacnica {
+
+	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
+
+}
---
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -0,0 +1,62 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+public class Valuta {
+
+	private String naziv;
+	private String skracenica;
+	private GregorianCalendar datum;
+	private double kupovni;
+	private double prodajni;
+	private double srednji;
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public String getSkracenica() {
+		return skracenica;
+	}
+
+	public void setSkracenica(String skracenica) {
+		this.skracenica = skracenica;
+	}
+
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+
+	public double getKupovni() {
+		return kupovni;
+	}
+
+	public void setKupovni(double kupovni) {
+		this.kupovni = kupovni;
+	}
+
+	public double getProdajni() {
+		return prodajni;
+	}
+
+	public void setProdajni(double prodajni) {
+		this.prodajni = prodajni;
+	}
+
+	public double getSrednji() {
+		return srednji;
+	}
+
+	public void setSrednji(double srednji) {
+		this.srednji = srednji;
+	}
+
+}

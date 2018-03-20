a1c2c33f073ee89ca4021eb85adb9dc81e818ec0
User <User@192.168.40.246>
2018-03-20 22:18:59 +0100

    I u klasi Menjacnica i u klasi Valuta su dodate metode hashCode(), toString(), equals()

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -6,4 +6,35 @@ public class Menjacnica {
 
 	private LinkedList<Valuta> valute = new LinkedList<Valuta>();
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((valute == null) ? 0 : valute.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Menjacnica other = (Menjacnica) obj;
+		if (valute == null) {
+			if (other.valute != null)
+				return false;
+		} else if (!valute.equals(other.valute))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Menjacnica [valute=" + valute + "]";
+	}
+
+	
 }
--- Menjacnica/src/menjacnica/Valuta.java
+++ Menjacnica/src/menjacnica/Valuta.java
@@ -59,4 +59,60 @@ public class Valuta {
 		this.srednji = srednji;
 	}
 
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skracenica=" + skracenica + ", datum=" + datum + ", kupovni=" + kupovni
+				+ ", prodajni=" + prodajni + ", srednji=" + srednji + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		temp = Double.doubleToLongBits(prodajni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((skracenica == null) ? 0 : skracenica.hashCode());
+		temp = Double.doubleToLongBits(srednji);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovni) != Double.doubleToLongBits(other.kupovni))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (Double.doubleToLongBits(prodajni) != Double.doubleToLongBits(other.prodajni))
+			return false;
+		if (skracenica == null) {
+			if (other.skracenica != null)
+				return false;
+		} else if (!skracenica.equals(other.skracenica))
+			return false;
+		if (Double.doubleToLongBits(srednji) != Double.doubleToLongBits(other.srednji))
+			return false;
+		return true;
+	}
+
 }
